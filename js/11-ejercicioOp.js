a47e2ed1982994db0ee88e7ef44daadf082ad7e2
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-03-22 19:57:35 -0300

    se agrega una funcion para ejecutar el juego y se la invoca

--- js/11-ejercicioOp.js
+++ js/11-ejercicioOp.js
@@ -16,5 +16,22 @@ if(jugadaUsuario == `PIEDRA`){
 }
 console.log(jugadaUsuario);
 
-
+function aJugar(jugadaUsuario, jugadaCompu){
+    if(jugadaUsuario === jugadaCompu){
+        return `Empate!`
+    } else if(jugadaUsuario == 1 && jugadaCompu == 2){
+        return `Ganó la máquina! El papel le gana a la piedra`
+    } else if(jugadaUsuario == 1 && jugadaCompu == 3){
+        return `Ganasteee! La piedra le gana a la tijera`
+    } else if(jugadaUsuario == 2 && jugadaCompu == 1){
+        return `Ganasteee! El papel le gana a la piedra`
+    } else if(jugadaUsuario == 2 && jugadaCompu == 3){
+        return `Ganó la máquina! La tijera le gana al papel`
+    } else if(jugadaUsuario == 3 && jugadaCompu == 1){
+        return `Ganó la máquina! La piedra le gana a la tijera`
+    } else if(jugadaUsuario == 3 && jugadaCompu == 2){
+        return `Ganasteee! La tijera le gana al papel`
+    }
+}
+alert(aJugar(jugadaUsuario, jugadaCompu))
 } while(confirm(`¿Desea seguir jugando?`))
