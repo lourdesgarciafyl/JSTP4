69f437ef2ab3e9d082f7ad193d485de2a5ce5166
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-03-24 21:29:10 -0300

    se crea la clase Rectangulo con sus propiedades y métodos

---
+++ js/3-ejercicio.js
@@ -0,0 +1,55 @@
+class Rectangulo{
+    constructor(base, altura){
+    this.ancho = base;
+    this.altura = altura;
+    }
+    set cambiarAncho(nuevoAncho){
+        this.ancho=nuevoAncho;
+    }
+    set cambiarAlto(nuevaAltura){
+        this.altura= nuevaAltura;
+    }
+    get mostrarAncho(){
+        return this.ancho;
+    }
+    get mostrarAlto(){
+        return this.altura;
+    }
+    get perimetro(){
+        return (this.ancho+this.altura)*2;
+    }
+    get area(){
+        return this.ancho*this.altura;
+    }
+    informacion(){
+        return `El rectangulo tiene un ancho de ${this.mostrarAncho}cm y un alto de ${this.mostrarAlto}cm.<br>`
+    }
+}
+
+const rectangulo1 = new Rectangulo(10, 5);
+const rectangulo2 = new Rectangulo(80, 45);
+
+console.log(rectangulo1);
+console.log(rectangulo2);
+
+//Rectangulo 1
+document.write(`<h3>Rectángulo 1</h3>`)
+document.write(rectangulo1.informacion())
+document.write(`El rectángulo tiene un perímetro de ${rectangulo1.perimetro}cm y un área de ${rectangulo1.area}cm.`)
+
+document.write(`<h4>Modificamos su ancho y altura</h4>`)
+rectangulo1.cambiarAlto=10;
+rectangulo1.cambiarAncho=30;
+document.write(rectangulo1.informacion())
+document.write(`El rectángulo tiene un perímetro de ${rectangulo1.perimetro}cm y un área de ${rectangulo1.area}cm.`)
+
+// Rectangulo 2
+document.write(`<h3>Rectángulo 2</h3>`)
+document.write(rectangulo2.informacion())
+document.write(`El rectángulo tiene un perímetro de ${rectangulo2.perimetro}cm y un área de ${rectangulo2.area}cm.`)
+
+document.write(`<h4>Modificamos su ancho y altura</h4>`)
+rectangulo2.cambiarAlto=548;
+rectangulo2.cambiarAncho=1259;
+document.write(rectangulo2.informacion())
+document.write(`El rectángulo tiene un perímetro de ${rectangulo2.perimetro}cm y un área de ${rectangulo2.area}cm.`)
