7792dc458da23bda657fba9204260cd41b888e20
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-03-27 21:18:19 -0300

    se agregan las clases Perro y Gato, queda pendiente el metodo emitir sonido

--- js/9-ejercicio.js
+++ js/9-ejercicio.js
@@ -1,7 +1,45 @@
 class Animal{
+    #nombre
+    #edad
     constructor(nombre, edad){
-        this.nombre = nombre;
-        this.edad = edad;
+        this.#nombre = nombre;
+        this.#edad = edad;
     }
-    emitirSonido()
+    get mostrarNombre(){
+        return this.#nombre
+    }
+    get mostrarEdad(){
+        return this.#edad
+    }
+    set cambiarNombre(nuevoNombre){
+        if(nuevoNombre.length > 0){
+            this.#nombre = nuevoNombre;
+        }
+    }
+    set cambiarEdad(nuevaEdad){
+        if(nuevaEdad.length > 0){
+            this.#edad = nuevaEdad;
+        }
+    }
+    emitirSonido(){
+        document.write(`Guau guau!`)
+    }
 }
+
+class Perro extends Animal{
+    constructor(nombre, edad){
+        super(nombre, edad)
+    }
+}
+
+class Gato extends Animal{
+    constructor(nombre, edad){
+        super(nombre, edad)
+    }
+}
+
+const firulais = new Perro(`Firulais`, 2);
+console.log(firulais.mostrarNombre);
+
+const vengador = new Gato(`Vengador`, 4);
+console.log(vengador.mostrarEdad)
